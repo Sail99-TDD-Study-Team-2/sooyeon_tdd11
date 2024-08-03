e9092cc3ec52181456f42bed8871f0404c880e44
Sooyeon <qjtmzj99@gmail.com>
2024-08-03 12:55:45 +0900

    111

---
+++ src/test/java/com/example/tdd_11/Tdd11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdd_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tdd11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
